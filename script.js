cb99fb0e818a09f84733e71b0e9a44ffc23e65e4
elajman <elajman2000@gmail.com>
2022-12-23 16:03:39 -0300

    Preentrega lajmanovich

---
+++ script.js
@@ -0,0 +1,62 @@
+'use strict'
+validoLogin()
+
+function validoLogin(){
+    let nombreUsuario 
+    let intentos = 0
+    let correoElectronico 
+    let clave 
+        
+        for (let i = 0; i < 3; i++) {
+            correoElectronico = prompt("Ingrese un correo electronico valido")
+            nombreUsuario = prompt("Ingrese nombre de usuario")
+            clave = prompt("Ingrese la clave")
+            intentos++
+        
+        if (!correoElectronico.match(/[^\s@]+@[^\s@]+\.[^\s@]+/gi) || nombreUsuario == '' || clave == null) {
+                alert("Por favor complete Correo, nombre y clave")
+                if (intentos == 3) {
+                    alert("Demasiados intentos, pruebe mas tarde")
+                }               
+            } else {
+                alert(`Bienvenido:  ${nombreUsuario} a nuestro sistema de Creditos Personales`)        
+                return 
+            }
+        }
+    }
+
+        let montoSolicitado = Number(prompt("Ingrese el Monto solicitado"))
+        let salario = Number(prompt("Ingreso el monto de sus ingresos mensuales que debe superar el credito solicitado"))
+       
+    function prestamoPersonal(){
+       
+ 
+        while (salario < montoSolicitado) {
+            alert("debe ingresar un monto menor a sus ingresos")
+            salario = Number(prompt("Vuela a ingresar un importe menor a sus ingresos"))
+        }
+
+            let plazo = Number(prompt("Ingrese el plazo en cantidad de meses para la devolucion"))
+                alert(`El Monto solicitado es  $${montoSolicitado} a devolver en ${plazo} Meses, si es correcto haga click en Ok para continuar.`)
+            const porcentajeInteres = 10
+            let interes = Number(montoSolicitado) * Number(porcentajeInteres) / 100
+            
+            let interesTotal = Number(interes) * Number(plazo) 
+            let valorTotalDevolucion = Number(montoSolicitado) + Number(interesTotal)
+                alert(`el Interes mensual es $${interes} y el interes total es: $${interesTotal}`)
+            let valorCuota = Number(valorTotalDevolucion) / Number(plazo) 
+                alert("Por Favor verifique los datos y si esta deacuerdo haga click en Ok.\n El Monto inicial solicitado es de: $" + montoSolicitado + "\n El monto total a devolver es: $" + valorTotalDevolucion + "\n El valor de la cuata mensual es de: $" + valorCuota.toFixed(2))
+
+    }
+    prestamoPersonal()
+
+
+
+
+
+
+
+
+
+
+
