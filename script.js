b45bc369b9c733e30d4fca5dca80c960dfff6b55
elajman <elajman2000@gmail.com>
2023-01-13 21:56:15 -0300

    2da prentrega lajmanovich

--- script.js
+++ script.js
@@ -1,66 +1,122 @@
-'use strict'
-validoLogin()
+    
+let relojes = [
+    {id: 5001, 
+        categoria: "digitales", 
+        marca: "casio", 
+        modelo: "gshok", 
+        precio: 60000, 
+        stock: 30, 
+        condicion: "nuevo"
+    },
+    {id: 5002, 
+        categoria: "digitales", 
+        marca: "citizen",
+        modelo: "jg", 
+        precio: 40000, 
+        stock: 28, 
+        condicion: "nuevo"
+    },
+    {id: 5003, 
+        categoria: "digitales", 
+        marca: "orient", 
+        modelo: "jmp", 
+        precio: 40000, 
+        stock: 30, 
+        condicion: "nuevo"
+    },
+    {id: 6001, 
+        categoria: "mecanicos", 
+        marca: "hamilton", 
+        modelo: "Mechanical", 
+        precio: 250000, 
+        stock: 18, 
+        condicion: "nuevo"
+    },
+    {id: 6002, 
+        categoria: "mecanicos", 
+        marca: "longines", 
+        modelo: "apline", 
+        precio: 120000, 
+        stock: 2, 
+        condicion: "usado"
+    },
+    {id: 7001, 
+        categoria: "automaticos", 
+        marca: "patek Philippe", 
+        modelo: "nautilus", 
+        precio: 38300000, 
+        stock: 1, 
+        condicion: "usado"
+    },
+    {id: 7002, 
+        categoria: "automaticos", 
+        marca: "audemars Piget", 
+        modelo: "royal", 
+        precio: 1050000, 
+        stock: 4, 
+        condicion: "nuevo"
+    },
+    {id: 7003, 
+        categoria: "automaticos", 
+        marca: "vacheron Constantin", 
+        modelo: "overseas", 
+        precio: 750000, 
+        stock: 7, 
+        condicion: "nuevo"
+    },
+    {id: 7004, 
+        categoria: "automaticos", 
+        marca: "a. lange & sonhe", 
+        modelo: "datagraph", 
+        precio: 3500000, 
+        stock: 2, 
+        condicion: "nuevo"
+    },
+    {id: 7005, 
+        categoria: "automaticos", 
+        marca: "omega", 
+        modelo: "seamaster", 
+        precio: 600000, 
+        stock: 10, 
+        condicion: "nuevo"}
+]
 
-function validoLogin(){
-    let nombreUsuario 
-    let intentos = 0
-    let correoElectronico 
-    let clave 
-        
-        for (let i = 0; i < 3; i++) {
-            correoElectronico = prompt("Ingrese un correo electronico valido")
-            nombreUsuario = prompt("Ingrese nombre de usuario")
-            clave = prompt("Ingrese la clave")
-            intentos++
-        
-        if (!correoElectronico.match(/[^\s@]+@[^\s@]+\.[^\s@]+/gi) || nombreUsuario == '' || clave == null) {
-                alert("Por favor complete Correo, nombre y clave")
-                if (intentos == 3) {
-                    alert("Demasiados intentos, pruebe mas tarde")
-                }               
-            } else {
-                alert(`Bienvenido:  ${nombreUsuario} a nuestro sistema de Creditos Personales`)        
-                return 
-            }
-        }
-    }
 
+alert('Bienvenido a nuestra tienda Online, haga click para vel los productos en stock')
+
+function tienProductos(){
+        let seleccionoProducto = []
+        let nuevoArray = []
+        let seleccionMarca = prompt(`Por favor seleccione una MARCA la lista de productos: /n${JSON.stringify(relojes)}`)
+        if (!seleccionMarca) {
+            alert('Ingrese un poducta de la list')
+            seleccionMarca = prompt(`Por favor seleccione una MARCA la lista de productos: /n${JSON.stringify(relojes)}`) 
+        } 
         
-       
-    function prestamoPersonal(){
-        let montoSolicitado = Number(prompt("Ingrese el Monto solicitado"))   
-            if (montoSolicitado <= 0) {
-                alert("Ingrese un monto superior a 0")
-                montoSolicitado = Number(prompt("Vuelva a ingresar el Monto solicitado"))
-            }
-        let salario = Number(prompt("Ingreso el monto de sus ingresos mensuales que debe superar el credito solicitado"))
-            while (salario < montoSolicitado) {
-            alert("debe ingresar un monto menor a sus ingresos")
-            salario = Number(prompt("Vuela a ingresar un importe menor a sus ingresos"))
-                
-            }
+        seleccionoProducto = relojes.filter(reloj => reloj.marca === seleccionMarca)
+        nuevoArray = seleccionoProducto.map(reloj =>{
+                return {
+                    marca: reloj.marca,
+                    modelo: reloj.modelo,
+                    precio: reloj.precio
+                }
+            }) 
+     
+        console.log(nuevoArray)
+        alert("Selecciono: " + JSON.stringify(nuevoArray) + 'si desea comprar el producto haga click Ok')
+        
+        let correoElectronico = prompt("Ingrese un correo electronico valido")
             
-            let plazo = Number(prompt("Ingrese el plazo en cantidad de meses para la devolucion"))
-                alert(`El Monto solicitado es  $${montoSolicitado} a devolver en ${plazo} Meses, si es correcto haga click en Ok para continuar.`)
-            const porcentajeInteres = 10
-            let interes = Number(montoSolicitado) * Number(porcentajeInteres) / 100
-            
-            let interesTotal = Number(interes) * Number(plazo) 
-            let valorTotalDevolucion = Number(montoSolicitado) + Number(interesTotal)
-                alert(`el Interes mensual es $${interes} y el interes total es: $${interesTotal}`)
-            let valorCuota = Number(valorTotalDevolucion) / Number(plazo) 
-                alert("Por Favor verifique los datos y si esta deacuerdo haga click en Ok.\n El Monto inicial solicitado es de: $" + montoSolicitado + "\n El monto total a devolver es: $" + valorTotalDevolucion + "\n El valor de la cuata mensual es de: $" + valorCuota.toFixed(2))
-
+        if (!correoElectronico.match(/[^\s@]+@[^\s@]+\.[^\s@]+/gi)) {
+                alert('ingrese un correo valido')
+                correoElectronico = prompt("Ingrese un correo electronico valido")           
+            } 
+        alert(`Gracias por haber comprado` + JSON.stringify(nuevoArray) + `Le sera enviado un mail con la orden de compra para abonar producto a: ${correoElectronico}`)
+        
     }
-    prestamoPersonal()
-
-
-
-
-
-
-
-
+        
+    tienProductos()
+    
 
 
 
